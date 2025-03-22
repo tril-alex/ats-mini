67b343822fe5e57b45bcd5cb1143e5911c6ed3ef
Max Arnold <arnold.maxim@gmail.com>
2025-03-22 10:25:01 +0700

    Get rid of misleading TFT_eSPI definitions

--- ats-mini/User_Setup.h
+++ ats-mini/User_Setup.h
@@ -1,10 +1,1 @@
-#define USER_SETUP_LOADED
-#define ST7789_DRIVER
-#define TFT_CS          5
-#define TFT_DC          17
-#define TFT_RST         16
-#define SPI_FREQUENCY   7500000
-#define SMOOTH_FONT
-#define TFT_RGB_ORDER TFT_BGR  // Colour order Blue-Green-Red
-
 #include <User_Setups/Setup206_LilyGo_T_Display_S3.h>     // For the LilyGo T-Display S3 based ESP32S3 with ST7789 170 x 320 TFT
