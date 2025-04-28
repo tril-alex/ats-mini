146e19b1ee784d38d6977c3b74a90643ce8ead95
Max Arnold <arnold.maxim@gmail.com>
2025-04-28 13:58:52 +0700

    Set the same default steps for each band as before

--- ats-mini/Common.h
+++ ats-mini/Common.h
@@ -57,9 +57,9 @@ typedef struct
   uint8_t bandType;       // Band type (FM, MW, or SW)
   uint8_t bandMode;       // Band mode (FM, AM, LSB, or USB)
   uint16_t minimumFreq;   // Minimum frequency of the band
-  uint16_t maximumFreq;   // maximum frequency of the band
+  uint16_t maximumFreq;   // Maximum frequency of the band
   uint16_t currentFreq;   // Default frequency or current frequency
-  int8_t currentStepIdx;  // Index of stepAM[]: defeult frequency step (see stepAM[])
+  int8_t currentStepIdx;  // Default frequency step
   int8_t bandwidthIdx;    // Index of the table bandwidthFM, bandwidthAM or bandwidthSSB;
   int16_t bandCal;        // Calibration value
 } Band;
--- ats-mini/Menu.cpp
+++ ats-mini/Menu.cpp
@@ -20,9 +20,9 @@ int bandIdx = 0;
 // Band limits are expanded to align with the nearest tuning scale mark
 Band bands[] =
 {
-  {"VHF",  FM_BAND_TYPE, FM,   6400, 10800, 10390, 1, 0, 0},
+  {"VHF",  FM_BAND_TYPE, FM,   6400, 10800, 10390, 2, 0, 0},
   // All band. LW, MW and SW (from 150kHz to 30MHz)
-  {"ALL",  SW_BAND_TYPE, AM,    150, 30000, 15000, 0, 4, 0},
+  {"ALL",  SW_BAND_TYPE, AM,    150, 30000, 15000, 1, 4, 0},
   {"11M",  SW_BAND_TYPE, AM,  25600, 26100, 25850, 1, 4, 0},
   {"13M",  SW_BAND_TYPE, AM,  21500, 21900, 21650, 1, 4, 0},
   {"15M",  SW_BAND_TYPE, AM,  18900, 19100, 18950, 1, 4, 0},
@@ -44,15 +44,15 @@ Band bands[] =
   {"MW3",  MW_BAND_TYPE, AM,   1700,  3500,  2500, 1, 4, 0},
   {"MW2",  MW_BAND_TYPE, AM,    495,  1701,   783, 2, 4, 0},
   {"MW1",  MW_BAND_TYPE, AM,    150,  1800,   810, 3, 4, 0},
-  {"160M", MW_BAND_TYPE, LSB,  1800,  2000,  1900, 0, 4, 0},
-  {"80M",  SW_BAND_TYPE, LSB,  3500,  4000,  3800, 0, 4, 0},
-  {"40M",  SW_BAND_TYPE, LSB,  7000,  7300,  7150, 0, 4, 0},
-  {"30M",  SW_BAND_TYPE, LSB, 10000, 10200, 10125, 0, 4, 0},
-  {"20M",  SW_BAND_TYPE, USB, 14000, 14400, 14100, 0, 4, 0},
-  {"17M",  SW_BAND_TYPE, USB, 18000, 18200, 18115, 0, 4, 0},
-  {"15M",  SW_BAND_TYPE, USB, 21000, 21500, 21225, 0, 4, 0},
-  {"12M",  SW_BAND_TYPE, USB, 24800, 25000, 24940, 0, 4, 0},
-  {"10M",  SW_BAND_TYPE, USB, 28000, 29700, 28500, 0, 4, 0},
+  {"160M", MW_BAND_TYPE, LSB,  1800,  2000,  1900, 5, 4, 0},
+  {"80M",  SW_BAND_TYPE, LSB,  3500,  4000,  3800, 5, 4, 0},
+  {"40M",  SW_BAND_TYPE, LSB,  7000,  7300,  7150, 5, 4, 0},
+  {"30M",  SW_BAND_TYPE, LSB, 10000, 10200, 10125, 5, 4, 0},
+  {"20M",  SW_BAND_TYPE, USB, 14000, 14400, 14100, 5, 4, 0},
+  {"17M",  SW_BAND_TYPE, USB, 18000, 18200, 18115, 5, 4, 0},
+  {"15M",  SW_BAND_TYPE, USB, 21000, 21500, 21225, 5, 4, 0},
+  {"12M",  SW_BAND_TYPE, USB, 24800, 25000, 24940, 5, 4, 0},
+  {"10M",  SW_BAND_TYPE, USB, 28000, 29700, 28500, 5, 4, 0},
   // https://www.hfunderground.com/wiki/CB
   {"CB",   SW_BAND_TYPE, AM,  25000, 30000, 27135, 0, 4, 0},
 };
@@ -213,7 +213,7 @@ static const Step amSteps[] =
 };
 
 static const Step *steps[4] = { fmSteps, ssbSteps, ssbSteps, amSteps };
-static uint8_t stepIdx[4] = { 2, 4, 4, 3 };
+static uint8_t stepIdx[4] = { 2, 5, 5, 1 };
 
 const Step *getCurrentStep(bool fast)
 {
