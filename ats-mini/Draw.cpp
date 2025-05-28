d6f1c34a3f3ff98c4b20b970ebe67520757073e8
Max Arnold <arnold.maxim@gmail.com>
2025-05-28 17:39:34 +0700

    Expand the system info About page

--- ats-mini/Draw.cpp
+++ ats-mini/Draw.cpp
@@ -4,6 +4,9 @@
 #include "Storage.h"
 #include "Utils.h"
 #include "Menu.h"
+#include <LittleFS.h>
+#include <nvs.h>
+#include <nvs_flash.h>
 
 // Display position control
 #define MENU_OFFSET_X    0    // Menu horizontal offset
@@ -103,12 +106,31 @@ static void drawAboutSystem(uint8_t arrow)
 
   sprintf(
     text,
-    "MEM: F %luM, R %luk (%luk), PS %luk (%luk)",
+    "FLASH: %luM, %luk (%luk), FS %luk (%luk)",
     ESP.getFlashChipSize() / (1024U * 1024U),
+    ESP.getFreeSketchSpace() / 1024U, (ESP.getFreeSketchSpace() - ESP.getSketchSize()) / 1024U,
+    LittleFS.totalBytes() / 1024U, (LittleFS.totalBytes() - LittleFS.usedBytes()) / 1024U
+  );
+  spr.drawString(text, 2, 70 + 16 * 0, 2);
+
+  nvs_stats_t nvs_stats;
+  nvs_get_stats(NULL, &nvs_stats);
+  sprintf(
+    text,
+    "NVS: TOTAL %lu, USED %lu, FREE %lu",
+    nvs_stats.total_entries,
+    nvs_stats.used_entries,
+    nvs_stats.free_entries
+  );
+  spr.drawString(text, 2, 70 + 16 * 1, 2);
+
+  sprintf(
+    text,
+    "MEM: HEAP %luk (%luk), PSRAM %luk (%luk)",
     ESP.getHeapSize()/1024U, ESP.getFreeHeap()/1024U,
     ESP.getPsramSize()/1024U, ESP.getFreePsram()/1024U
   );
-  spr.drawString(text, 2, 70 + 16 * 0, 2);
+  spr.drawString(text, 2, 70 + 16 * 2, 2);
 
   sprintf(
     text,
@@ -117,14 +139,10 @@ static void drawAboutSystem(uint8_t arrow)
     tft.readcommand8(ST7789_RDDST, 1),
     tft.readcommand32(ST7789_RDDST, 2)
   );
-  spr.drawString(text, 2, 70 + 16 * 1, 2);
+  spr.drawString(text, 2, 70 + 16 * 3, 2);
 
   sprintf(text, "WiFi MAC: %s", getMACAddress());
-  spr.drawString(text, 2, 70 + 16 * 2, 2);
-
-  // sprintf(text, "SDK: Arduino Core %d.%d.%d", ESP_ARDUINO_VERSION_MAJOR, ESP_ARDUINO_VERSION_MINOR, ESP_ARDUINO_VERSION_PATCH);
-  //FILESYSTEM.totalBytes();
-  //FILESYSTEM.usedBytes();
+  spr.drawString(text, 2, 70 + 16 * 4, 2);
 
   for(int i=0 ; i<8 ; i++)
   {
